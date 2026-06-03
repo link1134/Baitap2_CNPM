23cecd658446f0338bd9a41c19f17b65b69a803d
“link1134” <23130099@st.hcmuaf.edu.vn>
2026-06-03 21:04:49 +0700

    Chỉnh sửa overlay pause

--- Baitap2_CNPM/controller/StartMenuController.java
+++ Baitap2_CNPM/controller/StartMenuController.java
@@ -74,10 +74,14 @@ public class StartMenuController {
 		int rows = Integer.parseInt(startMenu.getRowField().getText());
 		if (rows <= 0) {
 			throw new IllegalArgumentException("Số hàng phải lớn hơn 0!");
+		} else if (rows > 50) {
+			throw new IllegalArgumentException("Số hàng tối đa là 50!");
 		}
 		int cols = Integer.parseInt(startMenu.getColField().getText());
 		if (cols <= 0) {
 			throw new IllegalArgumentException("Số cột phải lớn hơn 0!");
+		} else if (cols > 50) {
+			throw new IllegalArgumentException("Số cột tối đa là 50!");
 		}
 		int mines = Integer.parseInt(startMenu.getMineField().getText());
 		if (mines <= 0) {
@@ -86,6 +90,8 @@ public class StartMenuController {
 
 		if (mines >= rows * cols) {
 			throw new IllegalArgumentException("Số mìn không được lớn hơn hoặc bằng số hàng × số cột!");
+		} else if (mines > rows * cols * 0.8) {
+			throw new IllegalArgumentException("Số mìn không được vượt quá 80% số ô!");
 		}
 		return new GameConfig(rows, cols, mines);
 	}
